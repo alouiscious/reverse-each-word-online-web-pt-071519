abf1f63d4160f3f4df21ae5a74b9c1e727481fba
Renee Sims <info@reneesoul.com>
2019-07-28 20:07:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,28 +2,11 @@
 
 def reverse_each_word(string)
   new_array = string.split(" ")
-
   new_array.collect {|element| element.reverse!}
+  
   new_array.join(" ")
 end
 
 
-# def reverse_collect_word(string)
-
-#   new_array = string.split(" ")
-
-#   new_array.collect {|element| element.reverse!}
-#   new_array.join(" ")
 
 
-# end
-
-# def hamburger(toppings)
-#   toppings.collect do |topping|
-#     "I love #{topping} on my burgers"
-#   end
-# end
-# it 'uses collect' do
-#   expect_any_instance_of(Array).to receive(:collect).and_call_original
-#   reverse_each_word("Verifying that collect is being called.")
-# end
